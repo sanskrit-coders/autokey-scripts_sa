36d1802b39a8f1c9db04fe75cdbb4045b8e3dfcb
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-11-05 18:58:12 +0530

    CU

--- scripts/split_vyanjana.py
+++ scripts/split_vyanjana.py
@@ -3,15 +3,16 @@ text = clipboard.get_selection()
 if len(text) > 0:
   script = detect.detect(text)
   scheme = sanscript.SCHEMES[script]
-  final_svara = scheme.split_vyanjanas_and_svaras(text)[-1]
-  if len(text) > 1:
-    text_out = text[:-1] + scheme["virama"]["्"] + " " + final_svara
-  elif len(text) == 1:
-    text_out = text + scheme["virama"]["्"] + " " + final_svara
-  # dialog.info_dialog(title="Information", message=text_out)
-  old_clipped_value = clipboard.get_clipboard()
-  clipboard.fill_clipboard(text_out)
-  keyboard.send_keys("<ctrl>+v")
-  time.sleep(0.1)
-  clipboard.fill_clipboard(old_clipped_value)
-  clipboard.fill_selection("")
+  if hasattr(scheme, "split_vyanjanas_and_svaras"):
+      final_svara = scheme.split_vyanjanas_and_svaras(text)[-1]
+      if len(text) > 1:
+        text_out = text[:-1] + scheme["virama"]["्"] + " " + final_svara
+      elif len(text) == 1:
+        text_out = text + scheme["virama"]["्"] + " " + final_svara
+      # dialog.info_dialog(title="Information", message=text_out)
+      old_clipped_value = clipboard.get_clipboard()
+      clipboard.fill_clipboard(text_out)
+      keyboard.send_keys("<ctrl>+v")
+      time.sleep(0.1)
+      clipboard.fill_clipboard(old_clipped_value)
+clipboard.fill_selection("")
--- scripts/split_vyanjana_in_samaasa.py
+++ scripts/split_vyanjana_in_samaasa.py
@@ -1,22 +1,23 @@
 from indic_transliteration import detect, sanscript
 text = clipboard.get_selection()
 time.sleep(0.1)
-dialog.info_dialog(title="Information", message=text)
+# dialog.info_dialog(title="Information", message=text)
 if len(text) == 0:
   keyboard.send_keys("-")
 
 if len(text) > 0:
   script = detect.detect(text)
   scheme = sanscript.SCHEMES[script]
-  final_svara = scheme.split_vyanjanas_and_svaras(text)[-1]
-  if len(text) > 1:
-    text_out = text[:-1] + scheme["virama"]["्"] + "-" + final_svara
-  elif len(text) == 1:
-    text_out = text + scheme["virama"]["्"] + "-" + final_svara
-  # dialog.info_dialog(title="Information", message=text_out)
-  old_clipped_value = clipboard.get_clipboard()
-  clipboard.fill_clipboard(text_out)
-  keyboard.send_keys("<ctrl>+v")
-  time.sleep(0.1)
-  clipboard.fill_clipboard(old_clipped_value)
-  clipboard.fill_selection("")
+  if hasattr(scheme, "split_vyanjanas_and_svaras"):
+      final_svara = scheme.split_vyanjanas_and_svaras(text)[-1]
+      if len(text) > 1:
+        text_out = text[:-1] + scheme["virama"]["्"] + "-" + final_svara
+      elif len(text) == 1:
+        text_out = text + scheme["virama"]["्"] + "-" + final_svara
+      # dialog.info_dialog(title="Information", message=text_out)
+      old_clipped_value = clipboard.get_clipboard()
+      clipboard.fill_clipboard(text_out)
+      keyboard.send_keys("<ctrl>+v")
+      time.sleep(0.1)
+      clipboard.fill_clipboard(old_clipped_value)
+clipboard.fill_selection("")
